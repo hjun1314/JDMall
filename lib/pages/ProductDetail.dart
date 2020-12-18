9161168129305cc1369e6e580e3ac24f3b98a6b2
Jerry <177984267@qq.com>
2020-12-18 14:26:04 +0800

    商品详情顶部栏筛选按钮完毕

--- lib/pages/ProductDetail.dart
+++ lib/pages/ProductDetail.dart
@@ -10,80 +10,178 @@ class ProductDetailPage extends StatefulWidget {
 }
 
 class _ProductDetailPageState extends State<ProductDetailPage> {
+    final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  //商品列表
+  Widget _productDeatilWidget() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.only(top: ScreenAdaper.height(80)),
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return Column(
+            children: <Widget>[
+              Row(
+                children: [
+                  Container(
+                    width: ScreenAdaper.width(180),
+                    height: ScreenAdaper.height(180),
+                    child: Image.network(
+                        "https://www.itying.com/images/flutter/list2.jpg",
+                        fit: BoxFit.cover),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      height: ScreenAdaper.height(180),
+                      margin: EdgeInsets.only(left: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                              "戴尔(DELL)灵越3670 英特尔酷睿i5 高性能 台式电脑整机(九代i5-9400 8G 256G)",
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2),
+                          Row(
+                            children: <Widget>[
+                              Container(
+                                height: ScreenAdaper.height(36),
+                                margin: EdgeInsets.only(right: 10),
+                                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Color.fromRGBO(230, 230, 230, 0.9)),
+                                child: Text("4g"),
+                              ),
+                              Container(
+                                height: ScreenAdaper.height(36),
+                                margin: EdgeInsets.only(right: 10),
+                                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Color.fromRGBO(230, 230, 230, 0.9)),
+                                child: Text("111"),
+                              )
+                            ],
+                          ),
+                          Text("￥990",
+                              style:
+                                  TextStyle(color: Colors.red, fontSize: 16)),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Divider(height: 20)
+                ],
+              )
+            ],
+          );
+        },
+        itemCount: 11,
+      ),
+    );
+  }
+
+  //顶部刷选导航
+  Widget _subHeaderWidget() {
+    return Positioned(
+      top: 0,
+      height: ScreenAdaper.height(80),
+      width: ScreenAdaper.width(750),
+      child: Container(
+        width: ScreenAdaper.width(750),
+        height: ScreenAdaper.height(80),
+        decoration: BoxDecoration(
+            border: Border(
+                bottom: BorderSide(
+                    width: 1, color: Color.fromRGBO(233, 233, 233, 0.9)))),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: InkWell(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(
+                      0, ScreenAdaper.height(16), 0, ScreenAdaper.height(16)),
+                  child: Text(
+                    "综合",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.red),
+                  ),
+                ),
+                onTap: () {},
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: InkWell(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(
+                      0, ScreenAdaper.height(16), 0, ScreenAdaper.height(16)),
+                  child: Text(
+                    "销量",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.red),
+                  ),
+                ),
+                onTap: () {},
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: InkWell(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(
+                      0, ScreenAdaper.height(16), 0, ScreenAdaper.height(16)),
+                  child: Text(
+                    "价格",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.red),
+                  ),
+                ),
+                onTap: () {},
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: InkWell(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(
+                      0, ScreenAdaper.height(16), 0, ScreenAdaper.height(16)),
+                  child: Text(
+                    "筛选",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.red),
+                  ),
+                ),
+                onTap: () {
+                  //侧边栏
+                  _scaffoldKey.currentState.openEndDrawer();
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     ScreenAdaper.init(context);
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text("商品详情"),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: ListView.builder(
-          itemBuilder: (context, index) {
-            return Column(
-              children: <Widget>[
-                Row(
-                  children: [
-                    Container(
-                      width: ScreenAdaper.width(180),
-                      height: ScreenAdaper.height(180),
-                      child: Image.network(
-                          "https://www.itying.com/images/flutter/list2.jpg",
-                          fit: BoxFit.cover),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        height: ScreenAdaper.height(180),
-                        margin: EdgeInsets.only(left: 10),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                                "戴尔(DELL)灵越3670 英特尔酷睿i5 高性能 台式电脑整机(九代i5-9400 8G 256G)",
-                                overflow: TextOverflow.ellipsis,
-                                maxLines: 2),
-                            Row(
-                              children: <Widget>[
-                                Container(
-                                  height: ScreenAdaper.height(36),
-                                  margin: EdgeInsets.only(right: 10),
-                                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(10),
-                                      color:
-                                          Color.fromRGBO(230, 230, 230, 0.9)),
-                                  child: Text("4g"),
-                                ),
-                                Container(
-                                  height: ScreenAdaper.height(36),
-                                  margin: EdgeInsets.only(right: 10),
-                                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(10),
-                                      color:
-                                          Color.fromRGBO(230, 230, 230, 0.9)),
-                                  child: Text("111"),
-                                )
-                              ],
-                            ),
-                            Text("￥990",
-                                style:
-                                    TextStyle(color: Colors.red, fontSize: 16)),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Divider(height: 20)
-                  ],
-                )
-              ],
-            );
-          },
-          itemCount: 11,
-        ),
+      endDrawer: Drawer(
+        child: Container(
+          child: Text("实现筛选功能")),
+      ),
+      body: Stack(
+        children: [
+         _productDeatilWidget(),
+         _subHeaderWidget()],
       ),
     );
   }
