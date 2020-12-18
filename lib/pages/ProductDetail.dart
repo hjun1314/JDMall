dd2eb28795a5db316410d3b8385163ee9c927dda
Jerry <177984267@qq.com>
2020-12-18 11:37:02 +0800

    商品详情页面布局完毕

--- lib/pages/ProductDetail.dart
+++ lib/pages/ProductDetail.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import '../tools/ScreenAdaper.dart';
+
 class ProductDetailPage extends StatefulWidget {
   Map arguments;
-  ProductDetailPage({Key key,this.arguments}) : super(key: key);
+  ProductDetailPage({Key key, this.arguments}) : super(key: key);
 
   @override
   _ProductDetailPageState createState() => _ProductDetailPageState();
@@ -10,11 +12,79 @@ class ProductDetailPage extends StatefulWidget {
 class _ProductDetailPageState extends State<ProductDetailPage> {
   @override
   Widget build(BuildContext context) {
+    ScreenAdaper.init(context);
     return Scaffold(
       appBar: AppBar(
-       title: Text("跳转到商品详情列表"),
-      )
-      
+        title: Text("商品详情"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return Column(
+              children: <Widget>[
+                Row(
+                  children: [
+                    Container(
+                      width: ScreenAdaper.width(180),
+                      height: ScreenAdaper.height(180),
+                      child: Image.network(
+                          "https://www.itying.com/images/flutter/list2.jpg",
+                          fit: BoxFit.cover),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        height: ScreenAdaper.height(180),
+                        margin: EdgeInsets.only(left: 10),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                                "戴尔(DELL)灵越3670 英特尔酷睿i5 高性能 台式电脑整机(九代i5-9400 8G 256G)",
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 2),
+                            Row(
+                              children: <Widget>[
+                                Container(
+                                  height: ScreenAdaper.height(36),
+                                  margin: EdgeInsets.only(right: 10),
+                                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color:
+                                          Color.fromRGBO(230, 230, 230, 0.9)),
+                                  child: Text("4g"),
+                                ),
+                                Container(
+                                  height: ScreenAdaper.height(36),
+                                  margin: EdgeInsets.only(right: 10),
+                                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color:
+                                          Color.fromRGBO(230, 230, 230, 0.9)),
+                                  child: Text("111"),
+                                )
+                              ],
+                            ),
+                            Text("￥990",
+                                style:
+                                    TextStyle(color: Colors.red, fontSize: 16)),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Divider(height: 20)
+                  ],
+                )
+              ],
+            );
+          },
+          itemCount: 11,
+        ),
+      ),
     );
   }
 }
