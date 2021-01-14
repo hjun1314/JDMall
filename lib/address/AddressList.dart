990e3f89fe39a7ffa5e09f3b07da30d95f09c976
Jerry <177984267@qq.com>
2021-01-14 23:02:35 +0800

    删除收货地址

--- lib/address/AddressList.dart
+++ lib/address/AddressList.dart
@@ -65,6 +65,54 @@ class _AddressListPageState extends State<AddressListPage> {
     Navigator.pop(context);
   }
 
+//长按弹出框
+  _showAlertDialog(id) async {
+    var result = await showDialog(
+        barrierDismissible: false, //表示点击灰色背景的时候是否消失弹出框
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("提示信息!"),
+            content: Text("您确定要删除吗?"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("取消"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              FlatButton(
+                child: Text("确定"),
+                onPressed: () async {
+                  //执行删除操作
+                  this._delAddress(id);
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          );
+        });
+  }
+
+//长按删除
+  _delAddress(id)async{
+ List userinfo=await UserServices.getUserInfo();
+    var tempJson={
+      "uid":userinfo[0]["_id"],
+      "id":id,      
+      "salt":userinfo[0]["salt"]
+    };
+
+    var sign=SignServices.getSign(tempJson);    
+
+    var api = '${Config.domain}api/deleteAddress';
+    var response = await Dio().post(api,data:{
+          "uid":userinfo[0]["_id"],
+          "id":id,                  
+          "sign":sign
+    });   
+    this._getAddressList();   //删除收货地址完成后重新获取列表
+  }
   @override
   Widget build(BuildContext context) {
     ScreenAdaper.init(context);
@@ -85,19 +133,36 @@ class _AddressListPageState extends State<AddressListPage> {
                       leading: this.addressList[index]["default_address"] == 1
                           ? Icon(Icons.check, color: Colors.red)
                           : Text(""),
-                      title: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                              "${this.addressList[index]["name"]} ${this.addressList[index]["phone"]}"),
-                          SizedBox(height: 10),
-                          Text("${this.addressList[index]["address"]}")
-                        ],
+                      title: InkWell(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                                "${this.addressList[index]["name"]} ${this.addressList[index]["phone"]}"),
+                            SizedBox(height: 10),
+                            Text("${this.addressList[index]["address"]}")
+                          ],
+                        ),
+                        onTap: () {
+                          _changeDefaultAddress(this.addressList[index]["_id"]);
+                        },
+                        //长按删除
+                        onLongPress: () {
+                          _showAlertDialog(this.addressList[index]["_id"]);
+                        },
                       ),
-                      trailing: Icon(Icons.edit, color: Colors.blue),
-                      onTap: () {
-                        _changeDefaultAddress(this.addressList[index]["_id"]);
-                      },
+                      trailing: IconButton(
+                        icon: Icon(Icons.edit, color: Colors.blue),
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/addressEdit',
+                              arguments: {
+                                "id": this.addressList[index]["_id"],
+                                "name": this.addressList[index]["name"],
+                                "phone": this.addressList[index]["phone"],
+                                "address": this.addressList[index]["address"],
+                              });
+                        },
+                      ),
                     ),
                     Divider(height: 20),
                   ],
